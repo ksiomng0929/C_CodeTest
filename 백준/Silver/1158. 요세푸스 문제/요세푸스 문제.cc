e6a738143ac9446464e7ba4f29914e8a9e3878a1
ksiomng <nadana0929@gmail.com>
2023-02-16 16:52:42 +0900

    [Silver IV] Title: 요세푸스 문제, Time: 64 ms, Memory: 2152 KB -BaekjoonHub

---
+++ 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.cc
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <algorithm>
+#include <queue>
+
+using namespace std;
+
+int n, k, tmp;
+queue<int> q;
+
+int main() {
+    ios_base :: sync_with_stdio(false);
+    
+    cin >> n >> k;
+    
+    for(int i=1;i<=n;i++){
+        q.push(i);
+    }
+    
+    cout << '<';
+    
+    while(q.size() > 1){
+        for(int i=0;i<k-1;i++){
+            tmp = q.front();
+            q.push(tmp);
+            q.pop();
+        }
+        
+        tmp = q.front();
+        cout << tmp << ", ";
+        q.pop();
+    }
+    
+    tmp = q.front();
+    
+    cout << tmp << ">\n";
+    
+    
+    
+    return 0;
+}
