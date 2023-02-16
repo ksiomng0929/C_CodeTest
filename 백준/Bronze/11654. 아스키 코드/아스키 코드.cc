39b5d95c250ebd7ad37aecc482400e966fa2e9ff
ksiomng <nadana0929@gmail.com>
2023-02-16 16:33:28 +0900

    [Bronze V] Title: 아스키 코드, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int main(){
+    ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+    char input;
+    cin >> input;
+    cout << int(input) << "\n";
+    
+    return 0;
+}
