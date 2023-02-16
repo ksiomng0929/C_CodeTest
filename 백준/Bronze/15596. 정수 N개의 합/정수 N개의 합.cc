bce83d5f74355770a429f7f74eb407fb287d07db
ksiomng <nadana0929@gmail.com>
2023-02-16 16:21:19 +0900

    [Bronze II] Title: 정수 N개의 합, Time: 12 ms, Memory: 13740 KB -BaekjoonHub

---
+++ 백준/Bronze/15596. 정수 N개의 합/정수 N개의 합.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+long long sum(vector<int> &a){
+   long long total = 0;
+   for(int i=0; i<a.size(); i++) {
+   total += a[i];
+   }
+  return total;
+  }
