5bdff723bfd6afbf093ac27002f09c386560bbcc
ksiomng <nadana0929@gmail.com>
2023-02-16 16:37:32 +0900

    [Bronze V] Title: 사칙연산, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10869. 사칙연산/사칙연산.cc
@@ -0,0 +1,17 @@
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(int argc, char const *argv[]) {
+ 
+    int a, b;
+    cin >> a >> b;
+ 
+    cout << a + b << endl;
+    cout << a - b << endl;
+    cout << a * b << endl;
+    cout << a / b << endl;
+    cout << a % b << endl;
+    return 0;
+}
