9753d6debc1f6c4d148654d32cd159b57a971a9e
ksiomng <nadana0929@gmail.com>
2023-02-16 16:49:07 +0900

    [Bronze V] Title: 별 찍기 - 1, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {    
+    int A;
+    cin >> A;
+    
+    for (int i = 0; i < A; i++){
+        for (int j = 0; j <= i; j++){
+            cout << "*";
+        }
+        cout << "\n";
+    }
+    return 0;
+}
+
