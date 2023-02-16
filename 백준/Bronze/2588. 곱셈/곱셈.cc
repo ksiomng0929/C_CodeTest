e8fbee59237c72f4e3d35358dcdc839d9adb1a20
ksiomng <nadana0929@gmail.com>
2023-02-16 16:47:11 +0900

    [Bronze III] Title: 곱셈, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2588. 곱셈/곱셈.cc
@@ -0,0 +1,16 @@
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+ 
+    int a,b;
+    cin >> a >> b;
+    
+    cout << a * (b%10) << endl;
+    cout << a * (b/10%10) << endl;
+    cout << a * (b/100) << endl;
+    cout << a * b << endl;
+    return 0;
+}
