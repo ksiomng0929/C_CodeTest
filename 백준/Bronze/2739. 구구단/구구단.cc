cc4afc57e334e2ae29a90870fa345a664779d129
ksiomng <nadana0929@gmail.com>
2023-02-16 16:46:31 +0900

    [Bronze V] Title: 구구단, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2739. 구구단/구구단.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+
+int main() {
+	int n;
+	int i;
+
+	cin >> n;
+
+	for (i = 1; i < 10; i++) {
+		cout << n << " * " << i << " = " << n * i << "\n";
+	}
+	return 0;
+}
