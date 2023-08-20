ca40182d3944d789b8b243df27f04204be071800
ksiomng <nadana0929@gmail.com>
2023-08-20 23:56:05 +0900

    [level 1] Title: 문자열 내 p와 y의 개수, Time: 0.03 ms, Memory: 16.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12916. 문자열 내 p와 y의 개수/문자열 내 p와 y의 개수.swift
@@ -0,0 +1,13 @@
+func solution(_ s:String) -> Bool
+{
+    var ans:Bool = false
+    var p_cnt = 0
+    var y_cnt = 0
+    for c in s{
+        if c == "p" || c == "P" {p_cnt += 1}
+        else if c == "y" || c == "Y" {y_cnt += 1}
+    }
+    if p_cnt == y_cnt {ans = true}
+    else {ans=false}
+    return ans
+}
