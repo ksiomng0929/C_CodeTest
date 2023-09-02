15e0afb47b037530001f48a189dee919113d2213
ksiomng <nadana0929@gmail.com>
2023-09-02 20:42:03 +0900

    [level 1] Title: 하샤드 수, Time: 0.03 ms, Memory: 16.4 MB -BaekjoonHub

--- 프로그래머스/lv1/12947. 하샤드 수/하샤드 수.swift
+++ 프로그래머스/lv1/12947. 하샤드 수/하샤드 수.swift
@@ -1,3 +1,7 @@
 func solution(_ x:Int) -> Bool {
-    return x % String(x).reduce(0, {$0 + Int(String($1))!}) == 0 ? true : false
+    return x % String(x).reduce(0, {$0 + Int(String($1))!}) == 0
 }
+
+// func solution(_ x:Int) -> Bool {
+//     return x % String(x).reduce(0, {$0 + Int(String($1))!}) == 0 ? true : false
+// }
