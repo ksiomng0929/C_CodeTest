6bd22af91185cca88d6ca0db157a59b6f8e6b21a
ksiomng <nadana0929@gmail.com>
2023-09-03 19:47:28 +0900

    [level 1] Title: 음양 더하기, Time: 0.05 ms, Memory: 16.6 MB -BaekjoonHub

--- 프로그래머스/lv1/76501. 음양 더하기/음양 더하기.swift
+++ 프로그래머스/lv1/76501. 음양 더하기/음양 더하기.swift
@@ -1,3 +1,4 @@
+import Foundation
 func solution(_ absolutes:[Int], _ signs:[Bool]) -> Int {
     var cnt = 0
     for i in 0..<signs.count {
@@ -5,3 +6,7 @@ func solution(_ absolutes:[Int], _ signs:[Bool]) -> Int {
     }
     return cnt
 }
+
+// func solution(_ absolutes:[Int], _ signs:[Bool]) -> Int {
+//     return (0..<absolutes.count).map { signs[$0] ? absolutes[$0] : -absolutes[$0] }.reduce(0, +)
+// }
