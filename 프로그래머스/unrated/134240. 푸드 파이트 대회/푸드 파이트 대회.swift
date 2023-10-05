f5d6a68a7a5dd8053ccff01047c6b76bd7cf7f69
ksiomng <202024007@kangwon.ac.kr>
2023-10-05 18:49:57 +0900

    [unrated] Title: 푸드 파이트 대회, Time: 0.05 ms, Memory: 16.3 MB -BaekjoonHub

--- 프로그래머스/unrated/134240. 푸드 파이트 대회/푸드 파이트 대회.swift
+++ 프로그래머스/unrated/134240. 푸드 파이트 대회/푸드 파이트 대회.swift
@@ -1,16 +1,26 @@
 import Foundation
+//
+//< 처음풀이 >
+//func solution(_ food:[Int]) -> String {
+//    var arr = [0]
+//    var answer = ""
+//    for i in (1..<food.count).reversed() {
+//        for _ in 0..<(food[i]/2) {
+//            arr.insert(i, at: arr.endIndex)
+//            arr.insert(i, at: arr.startIndex)
+//        }
+//    }
+//    for c in arr {
+//        answer += String(c)
+//    }
+//    return answer
+//}
 
+//리팩토링
 func solution(_ food:[Int]) -> String {
-    var arr = [0]
-    var answer = ""
-    for i in (1..<food.count).reversed() {
-        for _ in 0..<(food[i]/2) {
-            arr.insert(i, at: arr.endIndex)
-            arr.insert(i, at: arr.startIndex)
-        }
+    var result = ""
+    for i in 1..<food.count {
+        result += String(repeating: String(i), count: food[i] / 2)
     }
-    for c in arr {
-        answer += String(c)
-    }
-    return answer
+    return result + "0" + result.reversed()
 }
