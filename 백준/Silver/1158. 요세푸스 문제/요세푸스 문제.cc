53bb0842c17acb826a537fa32a229ded5f13133c
ksiomng <202024007@kangwon.ac.kr>
2023-11-02 21:35:04 +0900

    [Silver IV] Title: 요세푸스 문제, Time: 64 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.cc
+++ 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.cc
@@ -1,40 +1,33 @@
 #include <iostream>
-#include <algorithm>
 #include <queue>
-
+#include <vector>
 using namespace std;
 
-int n, k, tmp;
-queue<int> q;
+int main()
+{
+	int n, k;
+	cin >> n >> k;
+	deque<int> q;
+	deque<int> result;
 
-int main() {
-    ios_base :: sync_with_stdio(false);
-    
-    cin >> n >> k;
-    
-    for(int i=1;i<=n;i++){
-        q.push(i);
-    }
-    
-    cout << '<';
-    
-    while(q.size() > 1){
-        for(int i=0;i<k-1;i++){
-            tmp = q.front();
-            q.push(tmp);
-            q.pop();
-        }
-        
-        tmp = q.front();
-        cout << tmp << ", ";
-        q.pop();
-    }
-    
-    tmp = q.front();
-    
-    cout << tmp << ">\n";
-    
-    
-    
-    return 0;
+	for (int i = 1; i <= n; i++) {
+		q.push_back(i);
+	}
+
+	for (int i = 0; i < n; i++){
+		for (int j = 0; j< k-1; j++){
+			int a = q.front();
+			q.push_back(a);
+			q.pop_front();
+		}
+		result.push_back(q.front());
+		q.pop_front();
+	}
+
+	cout << "<";
+	for (int i = 0; i < n-1; i++) {
+		cout << result.front() << ", ";
+		result.pop_front();
+	}
+	cout << result.front() << ">";
 }
